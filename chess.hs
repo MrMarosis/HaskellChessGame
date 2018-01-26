b94872d62787a568ec3345c6d23557415a6db498
MrMarosis <bartlomiej.t.stachura@gmail.com>
2018-01-26 14:24:18 +0100

    Changed play func

--- chess.hs
+++ chess.hs
@@ -3,20 +3,27 @@ module Chess where
     import ChessBoard
     import BoardState
 
-
+    x = readBoard initialBoardStr
+    y = flattenBoard x 
+    z = (Piece White Queen)
+    z1 = Just z
+    d = addSquare z1 12 y 
+    e = groupToBoard d    
+    
+    main = do{
+        putStr $ showBoard x    
+        } 
     
     
-
-
-    play :: IO ()
-    play = do 
+    play board = do
+        putStr $ showBoard board 
         putStrLn "enter value of your piece: "
         s1 <- getLine
         let a = calculateIndex (s1 !! 0)(s1 !! 1)
         putStrLn "enter value for destination: " 
         d1<- getLine
         let b = calculateIndex (d1 !! 0)(d1 !! 1)
-        --makeMovewithColor White a b y
+        --makeMovewithColor White a b $ flattenBoard board
         putStrLn "enter value of your piece: "
         s2 <- getLine
         let a = calculateIndex (s2 !! 0)(s2 !! 1)
@@ -24,8 +31,7 @@ module Chess where
         d2<- getLine
         let b = calculateIndex (d2 !! 0)(d2 !! 1)
         --makeMovewithColor White a b y
-        print a 
-        print b
+        play board
 
 
         -- makeMove s b (flattenBoard $ readBoard initialBoardStr)
@@ -34,15 +40,6 @@ module Chess where
     
         
     --play :: Either Bool Board->Either Bool Board
-    --play (Left a) = (Left a) 
-    
-
-    x = readBoard initialBoardStr
-    y = flattenBoard x 
-    z = (Piece White Queen)
-    z1 = Just z
-    d = addSquare z1 12 y 
-    e = groupToBoard d
 
     --data Player = AI | Human
     
